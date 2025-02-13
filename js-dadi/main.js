f85139dcb9fd428fa3bd31ba624d1f5aaa88fca4
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-13 14:26:49 +0100

    create two variables with respective log

--- js-dadi/main.js
+++ js-dadi/main.js
@@ -8,12 +8,3 @@ let computerNumber = Math.floor(Math.random() * 6);
 console.log(myNumber);
 console.log(computerNumber);
 
-// Creare un condizionale if in cui si decreta chi è il vincitore
-
-if (myNumber > computerNumber) {
-    console.log("Hai vinto!");
-
-} else {
-    console.log("Hai perso");
-
-}
