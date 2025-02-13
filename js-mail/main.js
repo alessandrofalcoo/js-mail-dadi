9b2d557347a358a0241047a346c1f6f952ec2c3f
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-13 14:50:17 +0100

    create invited list

---
+++ js-mail/main.js
@@ -0,0 +1,11 @@
+/* Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . */
+
+// Creare un array con la lista degli invitati
+let invited = ['Paolo', 'Fabio', 'Pietro', 'Gianluca', 'Davide'];
+
+
+
+
